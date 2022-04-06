24d01d3f00532d6a841581b5e35091d85722360b
Kevin Labianco <klabianco@gmail.com>
2022-04-06 12:47:45 -0700

    update home

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,9 +3,33 @@ import Image from 'next/image'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
+import { MetaMaskProvider } from "metamask-react";
+import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        color: '#666666',
+      },
+      // styles for the `a`
+      a: {
+        color: 'blue.700',
+        _hover: {
+          textDecoration: 'underline',
+        },
+      },
+    },
+  },
+})
 
 const name = 'Kevin Labianco'
-export const siteTitle = 'Kevin Labianco - Next w/ react and typescript'
+export const siteTitle = 'Kevin Labianco - Web3 Software Engineer'
+
+const pfp = () => {
+  return 'https://pbs.twimg.com/profile_images/1510080932060942349/ggNHijDH_400x400.jpg';
+}
 
 export default function Layout({
   children,
@@ -15,65 +39,65 @@ export default function Layout({
   home?: boolean
 }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
-      <header className={styles.header}>
-        {home ? (
-          <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <Link href="/">
-              <a>
-                <Image
-                  priority
-                  src="/images/profile.jpg"
-                  className={utilStyles.borderCircle}
-                  height={108}
-                  width={108}
-                  alt={name}
-                />
-              </a>
-            </Link>
-            <h2 className={utilStyles.headingLg}>
+    <ChakraProvider theme={theme}>
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Kevin Labianco is a web3 software engineer, specializing in React, Typescript, and Solidity.  Contact: klabianco@gmail.com"
+          />
+          <meta
+            property="og:image"
+            content="{pfp}"
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <Image
+                loader={pfp}
+                src="me.png"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={144}
+                alt={name}
+              />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+          ) : (
+            <>
               <Link href="/">
-                <a className={utilStyles.colorInherit}>{name}</a>
+                <a>
+                  <Image
+                    loader={pfp}
+                    src="me.png"
+                    className={utilStyles.borderCircle}
+                    height={108}
+                    width={108}
+                    alt={name}
+                  />
+                </a>
               </Link>
-            </h2>
-          </>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/">
+                  <a className={utilStyles.colorInherit}>{name}</a>
+                </Link>
+              </h2>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">
+              <a>← Back to home</a>
+            </Link>
+          </div>
         )}
-      </header>
-      <main>{children}</main>
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
-    </div>
+      </div>
+    </ChakraProvider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,25 +5,10 @@ import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
 import { GetStaticProps } from 'next'
-import { extendTheme, ChakraProvider } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import { FaTwitter } from 'react-icons/fa';
 
-const theme = extendTheme({
-  styles: {
-    global: {
-      // styles for the `body`
-      body: {
-        color: '#666666',
-      },
-      // styles for the `a`
-      a: {
-        color: 'blue.700',
-        _hover: {
-          textDecoration: 'underline',
-        },
-      },
-    },
-  },
-})
+
 
 export default function Home({
   allPostsData
@@ -35,33 +20,22 @@ export default function Home({
   }[]
 }) {
   return (
-    <ChakraProvider theme={theme}>
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-      <p>Hi I'm Kevin, a software engineer getting into the web3 space.  Find me on twitter <a href="https://twitter.com/klabianco" target="_blank">@klabianco</a> I'm also on <a href='https://opensea.io/kevinl' target='_blank'>opensea</a>.</p><br></br>
-      <p>This site uses <a href="https://chakra-ui.com/" target="_blank">ChakraUI</a></p>
+        <p>GM! I'm Kevin, a software engineer in the web3 space focusing on react, typescript, and solidity smart contracts. Let's buidl!</p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+      <section className={utilStyles.headingMD} style={{marginTop: 10 + 'px'}}>
+        <a href='https://twitter.com/klabianco' target="_blank">
+        <Button colorScheme='twitter' leftIcon={<FaTwitter />} isFullWidth='true'>
+    Twitter
+  </Button>
+        </a>
       </section>
+
     </Layout>
-    </ChakraProvider>
   )
 }
 
